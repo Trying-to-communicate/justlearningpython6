edca8c5f8b6c15a540ed71d75e7b88ca61ba1d2b
Blue <purgekingsubaru@gmail.com]>
2022-02-08 21:27:12 +0530

    Initial commit

---
+++ sayaka6.py
@@ -0,0 +1,15 @@
+d1 = {}
+d2 = d2 = {"isla":"giftia",
+      "watanabe":"akari",
+      "sayaka":"danganronpa",
+      "Ai":{"type":"oshi no ko", "Ruby":"aqua", "D":"all time fav"}}
+d2["Hoshino"] = "The Legendary Idol"
+d2["monogatari"] = "Nadeko"
+del d2 ["monogatari"]
+#print(d2)
+#d3=d2.copy()
+#print(d3)
+d2.update({"002":"Dino"})
+print(d2)
+print(d2.keys())
+print(d2.items())
